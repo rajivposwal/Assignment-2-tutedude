ee6dcfbd84c40079e35720e3e0256df8106790fd
rajivposwal <142899456+rajivposwal@users.noreply.github.com>
2025-09-12 10:16:18 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,32 @@
+#task 1
+'''a = int(input("Enter a number: "))
+
+if a%2 == 0:
+    print("{a}  is an even number")
+else :
+    print( "{a} is an odd number")'''
+#task 2
+'''total = 0
+for i in range(1,51):
+    total += i
+ 
+    print("The sum of integer from 1 to 50 is: ",total)'''
+'''def add(a,b):
+    print(a+b)
+add(1,2)
+
+def add1(a,b):
+    c = a + b
+    return c
+
+ans = add(10,20)
+
+ans = add1(10,20)
+print(ans)'''
+
+'''def add(x,y):
+    return x + y
+def square(z):
+    return z * z
+square(add(6,1))
+print(result)'''
